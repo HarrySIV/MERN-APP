22c32216e889d70f8b7facc83b5ab3294f5b24ab
Harry Sanders <harry.sandersiv@yahoo.com>
2022-01-26 17:59:48 -0500

    removing unnecssary files and creating places page

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
 import styles from './PlaceItem.module.css';
+import Card from '../../shared/components/UIElements/Card';
+import Button from '../../shared/components/FormElements/Button/Button';
 
-const PlaceItem = props => {
-   return 
+const PlaceItem = (props) => {
+  return (
+    <li className={styles.placeitem}>
+      <Card className={styles.placeitem__content}>
+        <div className={styles.placeitem__image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={styles.placeitem__info}>
+          <h2>{props.title}</h2>
+          <h3>{props.address}</h3>
+          <p>{props.description}</p>
+        </div>
+        <div className={styles.placeitem__actions}>
+          <Button inverse>VIEW ON MAP</Button>
+          <Button to={`/places/${props.id}`}>EDIT</Button>
+          <Button danger>DELETE</Button>
+        </div>
+      </Card>
+    </li>
+  );
 };
 
 export default PlaceItem;
--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
-const UserPlaces = props => {
-   return 
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'A tall place in New York',
+    imgUrl: '#',
+    address: '20 W 34th St, New York, NY 10001, United States',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: 'u1',
+  },
+  {
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'A tall place in New York',
+    imgUrl: '#',
+    address: '20 W 34th St, New York, NY 10001, United States',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: 'u2',
+  },
+];
+
+const UserPlaces = (props) => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
